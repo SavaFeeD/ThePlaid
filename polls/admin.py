edb4c48c09ba8f95037d34c8708903737d37f789
SavaFeeD <shachnev.02@mail.ru>
2021-01-04 12:42:07 +0700

    first

---
+++ polls/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
